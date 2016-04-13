96c3cca23bde03bf0c74e5c0c2924742931904a3
Jose E. Nunez <je.nunez@yahoo.ca>
2016-04-12 22:36:30 -0400

    creating SparkConf and SparkContext

--- spcluster.scala
+++ spcluster.scala
@@ -1,4 +1,20 @@
+#!/usr/bin/env scala -deprecation -J-Xmx1g -J-XX:NewRatio=4 -Dorg.xerial.snappy.lib.name=libsnappyjava.jnilib -Dorg.xerial.snappy.tempdir=/tmp
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.clustering._
 
+
+object FindItem {
+  def main(cmdLineArgs: Array[String]) : Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("Clustering")
+    sparkConf.set("spark.ui.enabled", "false")
+
+    val sc = new SparkContext(sparkConf)
+
+    println("Loading file")
+    val data = sc.textFile("data/scf2013.ascii")
+
+  }
+
+}
