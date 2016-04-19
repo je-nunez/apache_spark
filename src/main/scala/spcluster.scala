5a8be7c09316dd46902d12d203cbc0b7f81e674e
Jose E. Nunez <je.nunez@yahoo.ca>
2016-04-18 22:32:57 -0400

    avoid an exception in Spark MLlib KMeans fastSquaredDistance(): 'requirement failed' (MLlib MLUtils.scala:330 as of Spark version 1.6.1)
    Exception ... org.apache.spark.SparkException: ...: java.lang.IllegalArgumentException: requirement failed
       at scala.Predef$.require(Predef.scala:221)
       at org.apache.spark.mllib.util.MLUtils$.fastSquaredDistance(MLUtils.scala:330)
       at org.apache.spark.mllib.clustering.KMeans$.fastSquaredDistance(KMeans.scala:595)
       at org.apache.spark.mllib.clustering.KMeans$$anonfun$findClosest$1.apply(KMeans.scala:569)
       at org.apache.spark.mllib.clustering.KMeans$$anonfun$findClosest$1.apply(KMeans.scala:563)

--- src/main/scala/spcluster.scala
+++ src/main/scala/spcluster.scala
@@ -21,6 +21,13 @@ object SpCluster {
 
     parsedData.saveAsTextFile("/tmp/filtered_copy_directory")
 
+    if (!validateRddForKMeans(parsedData)) {
+      System.err.println("There are vectors inside the RDD which have different dimensions.\n" +
+                         "All vectors must have the same dimensions.\nAborting.")
+      sc.stop()
+      System.exit(1)
+    }
+
     val clusters = trainKMeans(parsedData)
 
     // print the centers of the clusters returned by the training of KMeans
@@ -59,12 +66,53 @@ object SpCluster {
     parsedData
   }
 
-  def trainKMeans(rdd: RDD[LinAlgVector], numClusters: Int = 5, numIterations: Int = 30):
-    KMeansModel = {
+  def minMaxVectorInRdd(rdd: RDD[LinAlgVector]): (Int, Int) = {
+    var min = Int.MaxValue
+    var max = Int.MinValue
 
-      val clusters = KMeans.train(rdd, numClusters, numIterations)
-      // println("Clusters found. Class " + clusters.getClass.getName)
-      clusters
-    }
+    rdd.collect().foreach(v => {
+        val v_sz = v.size
+        if (v_sz < min) {
+          min = v_sz
+        } else if (v_sz > max) {
+          max = v_sz
+        }
+      }
+    )
+
+    (min, max)
+  }
+
+  def validateRddForKMeans(rdd: RDD[LinAlgVector]): Boolean = {
+
+    // Avoid an exception because the vectors inside the RDD have different size
+    // (as of the current version of Spark 1.6.1 as of April 18, 2016)
+    //
+    // Exception ... org.apache.spark.SparkException: ...: java.lang.IllegalArgumentException: requirement failed
+    //    at scala.Predef$.require(Predef.scala:221)
+    //    at org.apache.spark.mllib.util.MLUtils$.fastSquaredDistance(MLUtils.scala:330)
+    //    at org.apache.spark.mllib.clustering.KMeans$.fastSquaredDistance(KMeans.scala:595)
+    //    at org.apache.spark.mllib.clustering.KMeans$$anonfun$findClosest$1.apply(KMeans.scala:569)
+    //    at org.apache.spark.mllib.clustering.KMeans$$anonfun$findClosest$1.apply(KMeans.scala:563)
+    //
+    // The lines at MLUtils.scala:330 in the stack trace of the exception are:
+    //   329	    val n = v1.size
+    //   330	    require(v2.size == n)
+    // so vectors need to be the same size (in this case, it happens that one of the vectors is
+    // the calculated center of a KMeans cluster).
+
+    // get the minimum an maximum sizes for all vectors inside the RDD
+    val (min_size, max_size) = minMaxVectorInRdd(rdd)
+
+    (min_size == max_size)
+  }
+
+  def trainKMeans(rdd: RDD[LinAlgVector], numClusters: Int = 5, numIterations: Int = 30):
+      KMeansModel = {
+
+    val clusters = KMeans.train(rdd, numClusters, numIterations)
+    // println("Clusters found. Class " + clusters.getClass.getName)
+    clusters
+  }
 
 }
