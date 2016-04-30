ef8e8c3b3526af8543a5559db37a70dc12e9708f
Jose E. Nunez <je.nunez@yahoo.ca>
2016-04-29 22:08:15 -0400

    1st draft of a very simple Excel column filter in ETL to RDD

---
+++ src/main/scala/ExcelColumnFilter.scala
@@ -0,0 +1,30 @@
+// scalastyle:off
+
+package excel2rdd
+
+// Note: we do a very simple column filter. A more general column filter would
+// be like a
+// SELECT-<expression-list>-FROM-<tables-or-views>-WHERE-<boolean-expression-list>
+// in SQL. For an idea of different filters, there are pre-built filters in Weka,
+// that Spark of course also supports:
+//      http://weka.sourceforge.net/doc.dev/weka/filters/Filter.html (subclasses to get an idea)
+// This project doesn't need either generalization.
+
+abstract class ExcelColumnFilter {
+
+  def filterCol(colNumber: Int, colValue: String): String
+
+  def apply(colNumber: Int, colValue: String): String =
+    filterCol(colNumber, colValue)
+
+}
+
+case object ExcelDropColumns extends ExcelColumnFilter {
+
+  override def filterCol(colNumber: Int, colValue: String): String = {
+    // TO DO
+    "Not Implemented yet"
+  }
+
+}
+
--- src/main/scala/ExcelRowFilter.scala
+++ src/main/scala/ExcelRowFilter.scala
@@ -2,6 +2,21 @@
 
 package excel2rdd
 
+// Note: we do a very simple row filter to know how to handle the header
+// row in the Excel spreadsheet, and this is why we pass as parameter the
+// rowNumber. A more general row filter, which we don't need, would receive
+// two boolean functions, one for knowing when to execute the saveFunc(),
+// and another to know when to execute extractFunc(). In the most general
+// case, the row filter is like a match-case statement,
+// Array[XSSFRow => Boolean] for the guard cases, and another array
+// Array[XSSFRow => Unit] for the functions to be executed when their
+// corresponding guard case matches. For an idea of different filters, there
+// are pre-built filters in Weka, that Spark of course also supports:
+//      http://weka.sourceforge.net/doc.dev/weka/filters/Filter.html (subclasses to get an idea)
+// This project doesn't need this amplitude.
+
+// case.
+
 abstract class ExcelRowFilter {
 
   def filterRow(rowNumber: Int, rowValue: String,
