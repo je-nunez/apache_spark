b5fe31ad9c61d7bc850a9546fccb862c22762912
Jose E. Nunez <je.nunez@yahoo.ca>
2016-06-29 23:35:27 -0400

    another currying function in Scala to simplify ETL on Excel columns with ranges

--- src/main/scala/customNYFedBankSCE/NYFedBankSCERowTransform.scala
+++ src/main/scala/customNYFedBankSCE/NYFedBankSCERowTransform.scala
@@ -42,33 +42,33 @@ class NYFedBankSCERowTransform(val excelSpreadsh: Excel2RDD) extends ExcelRowTra
    */
 
   protected [this] val transformationList = Array(
-      transformColHQ17 _,
-      transformColHQ15 _,
-      transformColHQ14 _,
-      transformColHQ5b2 _,
-      transformColHQ5a _,
+      transformColCategorical("HQ17") _,
+      transformColCategorical("HQ15") _,
+      transformColCategorical("HQ14") _,
+      transformColCategorical("HQ5b2") _,
+      transformColCategorical("HQ5a") _,
       transformColYear("HQR6f") _,
       transformColYear("HQR6d") _,
-      transformColHQR2bnew _,
-      transformColHQR1b _,
+      transformColCategorical("HQR2bnew") _,
+      transformColCategorical("HQR1b") _,
       transformColYear("HQR1a") _,
       transformColYear("HQH11f") _,
       transformColYear("HQH11d") _,
       transformColHQH11b2 _,
       transformColYear("HQH11a") _,
       transformColHQH6a3 _,
-      transformCoHQH6a _,
+      transformColCategorical("HQH6a") _,
       transformColHQH5m3 _,
       transformColYear("HQH5k") _,
       transformColYear("HQH5i") _,
       transformColHQH5c2 _,
       transformColHQH2bnew _,
       transformColHQH2b _,
-      transformCoHQH1b _,
+      transformColCategorical("HQH1b") _,
       transformColYear("HQH1ab2") _,
       transformColYear("HQH1aa") _,
-      transformColHQH1 _,
-      transformColHQ4b1 _,
+      transformColCategorical("HQH1") _,
+      transformColCategorical("HQ4b1") _,
       transformColHQ1_1 _
 
       // HQ6c3 doesn't have a numerical category defined (no distance), but needs to be left as-is
@@ -78,15 +78,99 @@ class NYFedBankSCERowTransform(val excelSpreadsh: Excel2RDD) extends ExcelRowTra
 
    )
 
-  private [this] val mapYears = {
-    val tuples = ArrayBuffer.empty[(String, Int)]
-    tuples += (("0", 1910))
-    tuples += (("1959", 1955))         // "1959" means years before 1960
+  private [this] val mapYears: Map[String, String] = {
+    val tuples = ArrayBuffer.empty[(String, String)]
+    tuples += (("0", "1910"))
+    tuples += (("1959", "1955"))         // "1959" means years before 1960
     for { year <- 1960 to 2014 } {
-      tuples += ((year.toString, year))
+      val yearStr = year.toString
+      tuples += ((yearStr, yearStr))
     }
     tuples.toMap
   }
+
+  /**
+   * These are the middle values of the categories of values for some columns in the "2014 Housing
+   * Survey", from The Center for Microeconomic Data of the Federal Reserve of New York.
+   *
+   * For example, the column in that Excel spreadsheet named "HQ17" has these codes (left) and
+   * associated range of values:
+   *
+   *  Code-received for column "HQ17":
+   *  1                                 Less than $500
+   *  2                                 $500 to $999
+   *  3                                 $1000 to $1999
+   *  4                                 $2000 to $4999
+   *  5                                 $5000 to $9999
+   *  6                                 $10000 to $19999
+   *  7                                 $20000 to $29999
+   *  8                                 $30000 to $49999
+   *  9                                 $50000 to $99999
+   *  10                                $100000 to $249999
+   *  11                                $250000 to $499999
+   *  12                                $500000 to $749999
+   *  13                                $750000 to $999999
+   *  14                                $1000000 or more
+   *
+   * This is the input data we receive (column) to the left, but we can't apply K-Means to it
+   * because the distances won't have sense, so we convert the values in the left to the middle
+   * value of the range in the right. (An alternative to the middle value a small random noise
+   * can be added/substracted to the middle value, to be still in the range for the code received.)
+   *
+   * Note: Probably a less error-prone of the mapping of the categories to the ranges is possible,
+   *       for example, by parsing the Excel spreadsheet with their definitions (the only issue is
+   *       that it is free-text, so the parsing of those ranges to find its middle point might not
+   *       be clean either). "0" is a special value which doesn't appear in range, and we receive
+   *       it when there is no value for the cell in the Excel spreadsheet (ie., the cell has NA).
+   */
+
+  private [this] val middleValuesOfRangesColumn: Map[String, Map[String, String]] = Map(
+    "HQ17" -> Map("0" -> "0", "1" -> "250", "2" -> "750", "3" -> "1500", "4" -> "3500",
+                  "5" -> "7500", "6" -> "15000", "7" -> "25000", "8" -> "40000", "9" -> "75000",
+                  "10" -> "175000", "11" -> "375000", "12" -> "625000", "13" -> "875000",
+                  "14" -> "1000000"),
+
+    "HQ15" -> Map("0" -> "0", "1" -> "250", "2" -> "750", "3" -> "1500", "4" -> "3500",
+                  "5" -> "7500", "6" -> "15000", "7" -> "25000", "8" -> "40000", "9" -> "75000",
+                  "10" -> "100000"),
+
+        // There is a typo in the "2014 Housing Survey" Excel workbook for HQ14, because it says
+        // that the numerical ranges are:
+        //     1: [0, 620)
+        //     2: [620, 629]
+        //     3: [680, 719]
+        // so the 2 should be instead [620,679], and not the typo [620,629]
+    "HQ14" -> Map("0" -> "0", "1" -> "310", "2" -> "650", "3" -> "700", "4" -> "740",
+                  "5" -> "770", "6" -> "630"),
+
+    "HQ5b2" -> Map("0" -> "0.0", "1" -> "1.0", "2" -> "2.25", "3" -> "2.75", "4" -> "3.25",
+                   "5" -> "3.75", "6" -> "4.25", "7" -> "4.75", "8" -> "5.25", "9" -> "5.75",
+                   "10" -> "6.25", "11" -> "6.75", "12" -> "7.25", "13" -> "7.75", "14" -> "8.25"),
+
+    "HQ5a" -> Map("0" -> "0.0", "1" -> "1.0", "2" -> "2.25", "3" -> "2.75", "4" -> "3.25",
+                  "5" -> "3.75", "6" -> "4.25", "7" -> "4.75", "8" -> "5.25", "9" -> "5.75",
+                  "10" -> "6.25", "11" -> "6.75", "12" -> "7.25", "13" -> "7.75", "14" -> "8.25"),
+
+    "HQR2bnew" -> Map("0" -> "0.0", "1" -> "3", "2" -> "30", "3" -> "365",
+                      "4" -> s"${365 * 2}", "5" -> s"${365 * 3}", "6" -> s"${365 * 4}"),
+
+        // Here "7" is a little special, more like NA. Very probably this attribute should be
+        // treated the same way as column "HQ6c3"
+    "HQR1b" -> Map("0" -> "0", "1" -> "1", "2" -> "3", "3" -> "5", "4" -> "7", "5" -> "10",
+                   "6" -> "15", "7" -> "0"),
+
+    "HQH6a" -> Map("0" -> "0", "1" -> "3", "2" -> "9", "3" -> "18", "4" -> "30", "5" -> "40"),
+
+    "HQH1b" -> Map("0" -> "0", "1" -> "1", "2" -> "3", "3" -> "5", "4" -> "7", "5" -> "9",
+                   "6" -> "12", "7" -> "0"),
+
+    "HQH1" -> (Map("1" -> "0") ++ mapYears),   // HQH1's is almost "mapYears", except "1" -> "0",
+                                               // for "1" means "I/We did not buy this residence"
+
+    "HQ4b1" -> Map("0" -> "0", "1" -> "2", "2" -> "12", "3" -> "30", "4" -> "50")
+
+  )
+
 
   override def transformRow(rowNumber: Int, rowCells: Array[String]): Array[String] = {
     if (rowNumber == 0) {
@@ -114,120 +198,35 @@ class NYFedBankSCERowTransform(val excelSpreadsh: Excel2RDD) extends ExcelRowTra
 
     val idx = excelSpreadsh.findHeader(columnName)
     if (idx >= 0) {
-      row(idx) = mapYears(row(idx)).toString
+      row(idx) = mapYears(row(idx))
     }
   }
 
   /**
-   * Transform the value of the Excell cells HQ17, with nominal values, to reflect the same
-   * distances as the ranges it represents.
+   * A curried function to transform a categorical column in the "2014 Housing Survey", from The
+   * Center for Microeconomic Data of the Federal Reserve of New York, to the middle value of
+   * its range.
    */
 
-  private [this] def transformColHQ17(row: ArrayBuffer[String]): Unit = {
-    // probably a less error-prone of the mapping of the categories to the ranges is possible,
-    // for example, by parsing the Excel spreadsheet with their definitions (the only issue is
-    // that it is free-text, so the parsing of those ranges to find its middle point might not
-    // be clean either). "0" is a special value which doesn't appear in range, and we receive
-    // it when there is no value for HQ17 (ie., when HQ17 is NA).
+  private [this] def transformColCategorical(columnName: String)(row: ArrayBuffer[String]): Unit =
+  {
+    require(middleValuesOfRangesColumn.contains(columnName),
+            s"The map 'middleValuesOfRangesColumn' must have a definition for key '$columnName'")
 
-    val idx = excelSpreadsh.findHeader("HQ17")
+    val idx = excelSpreadsh.findHeader(columnName)
     if (idx >= 0) {
-      val realRangeMiddleMap = Map("0" -> 0, "1" -> 250, "2" -> 750, "3" -> 1500,
-                                   "4" -> 3500, "5" -> 7500, "6" -> 15000, "7" -> 25000,
-                                   "8" -> 40000, "9" -> 75000, "10" -> 175000,
-                                   "11" -> 375000, "12" -> 625000, "13" -> 875000,
-                                   "14" -> 1000000)
-      row(idx) = realRangeMiddleMap(row(idx)).toString
-    }
-  }
-
-  /**
-   * Transform the value of the Excell cells HQ15, with nominal values, to reflect the same
-   * distances as the ranges it represents.
-   */
-
-  private [this] def transformColHQ15(row: ArrayBuffer[String]): Unit = {
-
-    val idx = excelSpreadsh.findHeader("HQ15")
-    if (idx >= 0) {
-      val realRangeMiddleMap = Map("0" -> 0, "1" -> 250, "2" -> 750, "3" -> 1500,
-                                   "4" -> 3500, "5" -> 7500, "6" -> 15000, "7" -> 25000,
-                                   "8" -> 40000, "9" -> 75000, "10" -> 100000)
-      row(idx) = realRangeMiddleMap(row(idx)).toString
-    }
-  }
-
-  /**
-   * Transform the value of the Excell cells HQ14, with nominal values, to reflect the same
-   * distances as the ranges it represents.
-   */
-
-  private [this] def transformColHQ14(row: ArrayBuffer[String]): Unit = {
-
-    // There is a typo in the "2014 Housing Survey" Excel workbook for HQ14, from The Center
-    // for Microeconomic Data of the Federal Reserve of New York, because it says that the
-    // numerical ranges are:
-    //     1: [0, 620)
-    //     2: [620, 629]
-    //     3: [680, 719]
-    // so the 2 should be instead [620,679], and not the typo [620,629]
-
-    val idx = excelSpreadsh.findHeader("HQ14")
-    if (idx >= 0) {
-      val realRangeMiddleMap = Map("0" -> 0, "1" -> 310, "2" -> 650, "3" -> 700,
-                                   "4" -> 740, "5" -> 770, "6" -> 630)
-      row(idx) = realRangeMiddleMap(row(idx)).toString
-    }
-  }
-
-  /**
-   * Transform the value of the Excell cells HQ5b2, with nominal values, to reflect the same
-   * distances as the ranges it represents.
-   */
-
-  private [this] def transformColHQ5b2(row: ArrayBuffer[String]): Unit = {
-
-    val idx = excelSpreadsh.findHeader("HQ5b2")
-    if (idx >= 0) {
-      val realRangeMiddleMap = Map("0" -> "0.0", "1" -> "1.0", "2" -> "2.25", "3" -> "2.75",
-                                   "4" -> "3.25", "5" -> "3.75", "6" -> "4.25", "7" -> "4.75",
-                                   "8" -> "5.25", "9" -> "5.75", "10" -> "6.25", "11" -> "6.75",
-                                   "12" -> "7.25", "13" -> "7.75", "14" -> "8.25")
-      row(idx) = realRangeMiddleMap(row(idx))
-    }
-  }
-
-  private [this] def transformColHQ5a(row: ArrayBuffer[String]): Unit = {
-
-    val idx = excelSpreadsh.findHeader("HQ5a")
-    if (idx >= 0) {
-      val realRangeMiddleMap = Map("0" -> "0.0", "1" -> "1.0", "2" -> "2.25", "3" -> "2.75",
-                                   "4" -> "3.25", "5" -> "3.75", "6" -> "4.25", "7" -> "4.75",
-                                   "8" -> "5.25", "9" -> "5.75", "10" -> "6.25", "11" -> "6.75",
-                                   "12" -> "7.25", "13" -> "7.75", "14" -> "8.25")
-      row(idx) = realRangeMiddleMap(row(idx))
-    }
-  }
-
-  private [this] def transformColHQR2bnew(row: ArrayBuffer[String]): Unit = {
-
-    val idx = excelSpreadsh.findHeader("HQR2bnew")
-    if (idx >= 0) {
-      val realRangeMiddleMap = Map("0" -> "0.0", "1" -> 3, "2" -> 30, "3" -> 365,
-                                   "4" -> 365 * 2, "5" -> 365 * 3, "6" -> 365 * 4)
-      row(idx) = realRangeMiddleMap(row(idx)).toString
-    }
-  }
-
-  private [this] def transformColHQR1b(row: ArrayBuffer[String]): Unit = {
-
-    val idx = excelSpreadsh.findHeader("HQR1b")
-    if (idx >= 0) {
-      // Here "7" is a little special, more like NA. Very probably this attribute should be
-      // treated the same way as column "HQ6c3"
-      val realRangeMiddleMap = Map("0" -> 0, "1" -> 1, "2" -> 3, "3" -> 5,
-                                   "4" -> 7, "5" -> 10, "6" -> 15, "7" -> 0)
-      row(idx) = realRangeMiddleMap(row(idx)).toString
+      val middleValuesForColumn = middleValuesOfRangesColumn(columnName)
+      val originalValue = row(idx)
+      val translatedMiddleVal = middleValuesForColumn(originalValue)
+      // the new "translatedMiddleVal" corresponding to the input value "originalValue" must be a
+      // double, for the K-Means works on double values. (To verify this condition on the map
+      // "translatedMiddleVal" for several rows in the input Excel spreadsheet is inefficient,
+      // because it could verify the same value multiple times, for different rows in the
+      // input Excel spreadsheet.)
+      require(translatedMiddleVal.toDouble >= Double.MinValue,
+              s"The mapped value for code '$originalValue' in the column key '$columnName' " +
+              "must be a double")
+      row(idx) = translatedMiddleVal
     }
   }
 
@@ -295,16 +294,6 @@ class NYFedBankSCERowTransform(val excelSpreadsh: Excel2RDD) extends ExcelRowTra
       // column HQH6a3)
 
       row(idxHQH6a3) = excelSpreadsh.fillNANullValue.toString   // this value is "0" by default.
-    }
-  }
-
-  private [this] def transformCoHQH6a(row: ArrayBuffer[String]): Unit = {
-
-    val idx = excelSpreadsh.findHeader("HQH6a")
-    if (idx >= 0) {
-      val realRangeMiddleMap = Map("0" -> 0, "1" -> 3, "2" -> 9, "3" -> 18, "4" -> 30,
-                                   "5" -> 40)
-      row(idx) = realRangeMiddleMap(row(idx)).toString
     }
   }
 
@@ -448,40 +437,6 @@ class NYFedBankSCERowTransform(val excelSpreadsh: Excel2RDD) extends ExcelRowTra
         // range, so the distance calculation has a little more of meaning
         row(idxHQH2b) = middleValueHQH2b
       }
-    }
-  }
-
-  private [this] def transformCoHQH1b(row: ArrayBuffer[String]): Unit = {
-
-    val idx = excelSpreadsh.findHeader("HQH1b")
-    if (idx >= 0) {
-      // Here "7" is a little special, more like NA. Very probably this attribute should be
-      // treated the same way as column "HQH1b"
-      val realRangeMiddleMap = Map("0" -> 0, "1" -> 1, "2" -> 3, "3" -> 5, "4" -> 7,
-                                   "5" -> 9, "6" -> 12, "7" -> 0)
-      row(idx) = realRangeMiddleMap(row(idx)).toString
-    }
-  }
-
-  private [this] def transformColHQH1(row: ArrayBuffer[String]): Unit = {
-
-    val idx = excelSpreadsh.findHeader("HQH1")
-    if (idx >= 0) {
-      if (row(idx) != "1") {
-        row(idx) = mapYears(row(idx)).toString
-      } else {
-        // row(idx) == "1": meaning "I/We did not buy this residence"
-        row(idx) = "0"
-      }
-    }
-  }
-
-  private [this] def transformColHQ4b1(row: ArrayBuffer[String]): Unit = {
-
-    val idx = excelSpreadsh.findHeader("HQ4b1")
-    if (idx >= 0) {
-      val realRangeMiddleMap = Map("0" -> 0, "1" -> 2, "2" -> 12, "3" -> 30, "4" -> 50)
-      row(idx) = realRangeMiddleMap(row(idx)).toString
     }
   }
 
